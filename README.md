9450ed22cbda1ee2c2edebbabdd56578b3b733b6
Gwenaël Magnenat <g.magnenat@gmail.com>
2024-08-09 08:05:52 +0200

    Push to github

---
+++ README.md
@@ -0,0 +1,60 @@
+# Frontend Mentor - Tip calculator app solution
+
+This is a solution to the [Tip calculator app challenge on Frontend Mentor](https://www.frontendmentor.io/challenges/tip-calculator-app-ugJNGbJUX). Frontend Mentor challenges help you improve your coding skills by building realistic projects.
+
+## Table of contents
+
+- [Overview](#overview)
+  - [The challenge](#the-challenge)
+  - [Screenshot](#screenshot)
+  - [Links](#links)
+- [My process](#my-process)
+  - [Built with](#built-with)
+  - [What I learned](#what-i-learned)
+  - [Continued development](#continued-development)
+  - [Useful resources](#useful-resources)
+- [Author](#author)
+- [Acknowledgments](#acknowledgments)
+
+## Overview
+
+### The challenge
+
+Users should be able to:
+
+- View the optimal layout for the app depending on their device's screen size
+- See hover states for all interactive elements on the page
+- Calculate the correct tip and total cost of the bill per person
+
+### Screenshot
+
+![](./screenshot.jpg)
+
+### Links
+
+- Solution URL: [Add solution URL here](https://your-solution-url.com)
+- Live Site URL: [Add live site URL here](https://your-live-site-url.com)
+
+## My process
+
+### Built with
+
+- Semantic HTML5 markup
+- CSS custom properties
+- Flexbox
+- CSS Grid
+- Mobile-first workflow
+
+### What I learned
+
+### Continued development
+
+### Useful resources
+
+## Author
+
+- Website - [Gwenaël Magnenat](https://gmagnenat.com)
+- Frontend Mentor - [@gmagnenat](https://www.frontendmentor.io/profile/gmagnenat)
+- LinkedIn - [@gmagnenat](https://www.linkedin.com/in/gmagnenat)
+
+## Acknowledgments
---
+++ main.js
@@ -0,0 +1,140 @@
+"use strict";
+
+document.addEventListener("DOMContentLoaded", init);
+
+const form = document.querySelector(".form");
+const amount = document.querySelector(".form__input--amount");
+const people = document.querySelector(".form__input--n-people");
+const tipPerPerson = document.querySelector(".tip-amount");
+const totalPerPerson = document.querySelector(".grand-total");
+const radios = document.getElementsByName("tip");
+const customRadio = document.querySelector(".custom-tip-radio");
+const customText = document.querySelector(".form__input--custom-tip");
+const amountErrorMessage = document.querySelector(".error-amount");
+const tipErrorMessage = document.querySelector(".error-tip");
+const peopleErrorMessage = document.querySelector(".error-people");
+const resetButton = document.querySelector(".form__reset-button");
+
+form.addEventListener("submit", () => {
+  e.preventDefault();
+});
+
+function getSelectedTipValue() {
+  const selectedRadio = document.querySelector('input[name="tip"]:checked');
+  return selectedRadio
+    ? selectedRadio.classList.contains("custom-tip-radio")
+      ? parseFloat(customText.value) || 0
+      : parseFloat(selectedRadio.value) || 0
+    : 0;
+}
+
+function roundToUpper05(value) {
+  return (Math.ceil(value * 20) / 20).toFixed(2);
+}
+
+function validateInputs(amountVal, peopleVal) {
+  let isValid = true;
+
+  if (!isFinite(peopleVal) || peopleVal <= 0) {
+    showError(peopleErrorMessage, "Can't be zero");
+    isValid = false;
+  } else {
+    clearError(peopleErrorMessage);
+  }
+
+  if (!isFinite(amountVal) || amountVal <= 0) {
+    showError(amountErrorMessage, "Can't be zero");
+    isValid = false;
+  } else {
+    clearError(amountErrorMessage);
+  }
+
+  if (getSelectedTipValue() <= 0) {
+    showError(tipErrorMessage, "Select a tip.");
+    isValid = false;
+  } else {
+    clearError(tipErrorMessage);
+  }
+
+  return isValid;
+}
+
+function unselectRadios() {
+  radios.forEach((radio) => (radio.checked = false));
+}
+
+function showError(element, message) {
+  element.textContent = message;
+  element.setAttribute("aria-live", "polite");
+  element.focus();
+  tipPerPerson.textContent = "--";
+  totalPerPerson.textContent = "--";
+  resetButton.disabled = true;
+}
+
+function clearError(element) {
+  element.textContent = "";
+  element.removeAttribute("aria-live");
+
+  // Check if all errors are cleared to enable the reset button
+  if (
+    !amountErrorMessage.textContent &&
+    !peopleErrorMessage.textContent &&
+    !tipErrorMessage.textContent
+  ) {
+    resetButton.disabled = false;
+  }
+}
+
+function calculateAndDisplayAmounts() {
+  const amountVal = parseFloat(amount.value);
+  const peopleVal = parseInt(people.value, 10);
+
+  if (!validateInputs(amountVal, peopleVal)) {
+    return;
+  }
+
+  const selectedTip = getSelectedTipValue();
+  const tipTotal = (amountVal * selectedTip) / 100;
+  const newAmount = amountVal + tipTotal;
+  const tipValPerson = tipTotal / peopleVal;
+  const amountValPerson = newAmount / peopleVal;
+
+  tipPerPerson.textContent = `$${roundToUpper05(tipValPerson)}`;
+  totalPerPerson.textContent = `$${roundToUpper05(amountValPerson)}`;
+}
+
+function attachEventListeners() {
+  [amount, people, customText].forEach((input) =>
+    input.addEventListener("input", calculateAndDisplayAmounts)
+  );
+
+  customRadio.addEventListener("change", () => customText.focus());
+  customText.addEventListener("click", () => {
+    unselectRadios();
+    customRadio.checked = true;
+  });
+
+  radios.forEach((radio) =>
+    radio.addEventListener("change", () => {
+      customText.value = "";
+      calculateAndDisplayAmounts();
+    })
+  );
+
+  document.addEventListener("keydown", (event) => {
+    if (
+      event.key === "Tab" &&
+      !document.querySelector('input[name="tip"]:checked')
+    ) {
+      radios[0].checked = true;
+      calculateAndDisplayAmounts();
+    }
+  });
+}
+
+function init() {
+  people.value = 1;
+  attachEventListeners();
+  calculateAndDisplayAmounts();
+}
